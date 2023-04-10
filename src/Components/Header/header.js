d398cc730c99b15c047e9ce7b6ca446535eb90d6
Abuzar Rahim <abuzarrahim@192.168.1.7>
2023-04-11 02:09:36 +0500

    make search function

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState ,useCallback} from 'react'
 import './header.styles.css'
-const header = () => {
+import { YOUTUBE_SEARCH_API } from '../../Constants'
+const Header = () => {
+  const [searchQuery,setSearchQuery]=useState('');
+  const [data,setData]=useState([]);
+  const [showSearch,setShowSearch] = useState(false);
+  const getData=useCallback(()=>{
+
+    fetch(YOUTUBE_SEARCH_API+searchQuery).then(data=>{return data.json()}).then(data=>{setData(data) 
+      console.log(data)});
+  },[searchQuery]);
+  useEffect(()=>{
+    const interval=setTimeout(()=>{
+      getData();
+    },100);
+    return ()=>{clearTimeout(interval)}
+  },[getData])
   return (
+    
     <div className='header'>
       <div className='left-header'>
           <img className="icon" src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="} alt=""/>
           <img className="logo" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1024px-Logo_of_YouTube_%282015-2017%29.svg.png"} alt=""/>
       </div>
       <div className='search-header'>
-        <input type="text" placeholder="Search" className="search-input"/>
+        <input value={searchQuery}
+          type="text"
+          placeholder="Search" 
+          className="search-input"
+          onChange={(e)=>{setSearchQuery(e.target.value)}}
+          onFocus={()=>setShowSearch(true)}
+          onBlur={()=>setShowSearch(false)}
+          />
         <button className="search-button">Search</button>
+        {showSearch && <div className='suggestion-container'>
+            <ul className='ul-element'>
+              {data[1].map((d,i)=>(
+              <li className='list' key={i}>
+                <span className='search-span'>
+                  🔎
+                </span>
+                <span>
+                  {d}
+                </span>
+              </li>))}
+            </ul>
+        </div>}
       </div>
       <div className='user-header'>
-        <img alt="user" src={"https://www.vhv.rs/dpng/d/421-4213525_png-file-svg-single-user-icon-png-transparent.png"} className='icon'/>
+        <img alt="user" src={"https://www.vhv.rs/dpng/d/421-4213525_png-file-svg-single-user-icon-png-transparent.png"} className='icon-user'/>
       </div>
     </div>
   )
 }
 
-export default header
+export default Header
--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -1,8 +1,62 @@
 import React from 'react'
-
+import './sidebar.styles.css';
 const sidebar = () => {
   return (
-    <div>sidebar</div>
+    <div>
+      <div>
+        
+        <ul>
+          <li>Home</li>
+          <li>Shorts</li>
+          <li>Subscriptions</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Library</li>
+          <li>History</li>
+        </ul>
+      </div>
+      <div>
+        <div>Sign in to like videos,</div>
+        <div>comment and subscribe.</div>
+        <div >
+          <button className='sign-button'>
+            <img className='icon' src={"https://www.vhv.rs/dpng/d/421-4213525_png-file-svg-single-user-icon-png-transparent.png"} alt='user'/>
+            <span> Sign In</span>
+          </button>
+        </div>
+      </div>
+      <div>
+        <h3>Explore</h3>
+        <ul>
+          <li>Trending</li>
+          <li>Music</li>
+          <li>Gaming</li>
+          <li>News</li>
+          <li>Sport</li>
+        </ul>
+      </div>
+      <div>
+        <button>Browse Channels</button>
+      </div>
+      <div>
+        <h3>
+          More from YouTube
+        </h3>
+        <ul>
+          <li>YouTube Kids</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Settings</li>
+          <li>Report History</li>
+          <li>Help</li>
+          <li>Send Feedback</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
